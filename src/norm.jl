f60b0ed92b68a101e143551faabd7e0b5c671b48
Denis Baručić <barucden@fel.cvut.cz>
2024-10-04 10:43:11 +0200

    improve normalization

--- src/norm.jl
+++ src/norm.jl
@@ -2,11 +2,11 @@
 function normalize(x::Decimal; rounded::Bool=false)
     p = 0
     if x.c != 0
-        while x.c % 10^(p+1) == 0
+        while x.c % BigTen^(p+1) == 0
             p += 1
         end
     end
-    c = BigInt(x.c / 10^p)
+    c = BigInt(x.c / BigTen^p)
     q = (c == 0 && x.s == 0) ? 0 : x.q + p
     if rounded
         Decimal(x.s, abs(c), q)
